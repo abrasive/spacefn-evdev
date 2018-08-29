29f2c15088d04709eda5a7e276c8e270637f545c
James Laird-Wah <james@laird-wah.net>
2018-08-29 22:01:42 +1000

    Rewrite logic to use a timeout
    Now using a state machine to make it a bit easier to reason about.
    
    When Space is held down it goes into the DECIDE state.
    During DECIDE, any other key-down events are buffered.
    A key-up of a buffered key (ie press and release during DECIDE) results
    in a transition to SHIFT, as does holding down Space for 200ms.
    Releasing Space without either of these happening causes it to send
    space down and up, then replay any buffered key-down events. The latter
    is most important for comfort and not reordering keys...

--- spacefn.c
+++ spacefn.c
@@ -5,11 +5,47 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
+// Global device handles {{{1
 struct libevdev *idev;
 struct libevdev_uinput *odev;
+int fd;
 
+// Ordered unique key buffer {{{1
+#define MAX_BUFFER 8
+unsigned int buffer[MAX_BUFFER];
+unsigned int n_buffer = 0;
+
+static int buffer_contains(unsigned int code) {
+    for (int i=0; i<n_buffer; i++)
+        if (buffer[i] == code)
+            return 1;
+
+    return 0;
+}
+
+static int buffer_remove(unsigned int code) {
+    for (int i=0; i<n_buffer; i++)
+        if (buffer[i] == code) {
+            memcpy(&buffer[i], &buffer[i+1], (n_buffer - i - 1) * sizeof(*buffer));
+            n_buffer--;
+            return 1;
+        }
+    return 0;
+}
+
+static int buffer_append(unsigned int code) {
+    if (n_buffer >= MAX_BUFFER)
+        return 1;
+    buffer[n_buffer++] = code;
+    return 0;
+}
+
+// Key I/O functions {{{1
+// output {{{2
+#define V_RELEASE 0
+#define V_PRESS 1
+#define V_REPEAT 2
 static void send_key(unsigned int code, int value) {
-    printf("send: %d %d\n", code, value);
     libevdev_uinput_write_event(odev, EV_KEY, code, value);
     libevdev_uinput_write_event(odev, EV_SYN, SYN_REPORT, 0);
 }
@@ -26,6 +62,21 @@ static void send_repeat(unsigned int code) {
     send_key(code, 2);
 }
 
+// input {{{2
+static int read_one_key(struct input_event *ev) {
+    int err = libevdev_next_event(idev, LIBEVDEV_READ_FLAG_NORMAL | LIBEVDEV_READ_FLAG_BLOCKING, ev);
+    if (err) {
+        fprintf(stderr, "Failed: (%d) %s\n", -err, strerror(err));
+        return err;
+    }
+
+    if (ev->type != EV_KEY)
+        return -1;
+
+    return 0;
+}
+
+// Key mapping {{{1
 unsigned int key_map(unsigned int code) {
     switch (code) {
         case KEY_BRIGHTNESSDOWN:    // my magical escape button
@@ -56,117 +107,131 @@ unsigned int key_map(unsigned int code) {
     return 0;
 }
 
-int space_pressed = 0;
+// State functions {{{1
+enum {
+    IDLE,
+    DECIDE,
+    SHIFT,
+} state = IDLE;
 
-#define MAX_KEYS 8
-int n_shifted = 0;
-unsigned int shifted[MAX_KEYS];
-int any_shifted;
-
-// If key was shifted, remove it and return true.
-// If was not shifted, return false.
-static int pop_shifted_key(unsigned int code) {
-    for (int i=0; i<n_shifted; i++)
-        if (shifted[i] == code) {
-            shifted[i] = shifted[--n_shifted];
-            return 1;
-        }
-    return 0;
-}
-
-static int is_shifted_key(unsigned int code) {
-    for (int i=0; i<n_shifted; i++)
-        if (shifted[i] == code)
-            return 1;
-    return 0;
-}
-
-static void debug_shift(void) {
-    printf("n%d a%d", n_shifted, any_shifted);
-    for (int i=0; i<n_shifted; i++)
-        printf(" %x", shifted[i]);
-    printf("\n");
-}
-
-static void handle_press(unsigned int code) {
-    if (code == KEY_SPACE) {
-        space_pressed = 1;
-        return;
-    }
-
-    if (space_pressed && key_map(code) && n_shifted < MAX_KEYS) {
-        shifted[n_shifted++] = code;
-        any_shifted = 1;
-        code = key_map(code);
-    }
-
-    send_press(code);
-}
-
-static void handle_release(unsigned int code) {
-    if (code == KEY_SPACE) {
-        space_pressed = 0;
-        if (n_shifted) {
-            // release all keys
-            for (int i=0; i<n_shifted; i++)
-                send_release(shifted[i]);
-            n_shifted = 0;
-        }
-
-        if (!any_shifted) {
-            send_press(KEY_SPACE);
-            send_release(KEY_SPACE);
-        }
-
-        any_shifted = 0;
-
-        return;
-    }
-
-    if (pop_shifted_key(code))
-        code = key_map(code);
-
-    send_release(code);
-}
-
-static void handle_repeat(unsigned int code) {
-    if (code == KEY_SPACE)
-        return;
-
-    if (space_pressed && is_shifted_key(code))
-        code = key_map(code);
-
-    send_repeat(code);
-}
-
-static void do_one_event(void) {
+static void state_idle(void) {  // {{{2
     struct input_event ev;
-    int err = libevdev_next_event(idev, LIBEVDEV_READ_FLAG_NORMAL | LIBEVDEV_READ_FLAG_BLOCKING, &ev);
-    if (err) {
-        fprintf(stderr, "Failed: (%d) %s\n", -err, strerror(err));
-        return;
+    for (;;) {
+        while (read_one_key(&ev));
+
+        if (ev.code == KEY_SPACE && ev.value == V_PRESS) {
+            state = DECIDE;
+            return;
+        }
+
+        send_key(ev.code, ev.value);
     }
-
-    if (ev.type != EV_KEY)
-        return;
-
-    if (ev.value == 0)
-        handle_release(ev.code);
-    else if (ev.value == 1)
-        handle_press(ev.code);
-    else if (ev.value == 2)
-        handle_repeat(ev.code);
-
-    debug_shift();
 }
 
-int main(int argc, char **argv) {
+static void state_decide(void) {    // {{{2
+    n_buffer = 0;
+    struct input_event ev;
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 200000;
+    fd_set set;
+    FD_ZERO(&set);
+
+    while (timeout.tv_usec >= 0) {
+        FD_SET(fd, &set);
+        int nfds = select(fd+1, &set, NULL, &set, &timeout);
+        if (!nfds)
+            break;
+
+        while (read_one_key(&ev));
+
+        if (ev.value == V_PRESS) {
+            buffer_append(ev.code);
+            continue;
+        }
+
+        if (ev.code == KEY_SPACE && ev.value == V_RELEASE) {
+            send_key(KEY_SPACE, V_PRESS);
+            send_key(KEY_SPACE, V_RELEASE);
+            for (int i=0; i<n_buffer; i++)
+                send_key(buffer[i], V_PRESS);
+            state = IDLE;
+            return;
+        }
+
+        if (ev.value == V_RELEASE && !buffer_contains(ev.code)) {
+            send_key(ev.code, ev.value);
+            continue;
+        }
+
+        if (ev.value == V_RELEASE && buffer_remove(ev.code)) {
+            unsigned int code = key_map(ev.code);
+            send_key(code, V_PRESS);
+            send_key(code, V_RELEASE);
+            state = SHIFT;
+            return;
+        }
+    }
+
+    printf("timed out\n");
+    state = SHIFT;
+}
+
+static void state_shift(void) {
+    n_buffer = 0;
+    struct input_event ev;
+    for (;;) {
+        while (read_one_key(&ev));
+
+        if (ev.code == KEY_SPACE && ev.value == V_RELEASE) {
+            for (int i=0; i<n_buffer; i++)
+                send_key(buffer[i], V_RELEASE);
+            state = IDLE;
+            return;
+        }
+        if (ev.code == KEY_SPACE)
+            continue;
+
+        unsigned int code = key_map(ev.code);
+        if (code) {
+            if (ev.value == V_PRESS)
+                buffer_append(code);
+            else if (ev.value == V_RELEASE)
+                buffer_remove(code);
+
+            send_key(code, ev.value);
+        } else {
+            send_key(ev.code, ev.value);
+        }
+
+    }
+}
+
+static void run_state_machine(void) {
+    for (;;) {
+        printf("state %d\n", state);
+        switch (state) {
+            case IDLE:
+                state_idle();
+                break;
+            case DECIDE:
+                state_decide();
+                break;
+            case SHIFT:
+                state_shift();
+                break;
+        }
+    }
+}
+
+
+int main(int argc, char **argv) {   // {{{1
     if (argc < 2) {
         printf("usage: %s /dev/input/...", argv[0]);
         return 1;
     }
 
-    int fd = open(argv[1], O_RDONLY);
+    fd = open(argv[1], O_RDONLY);
     if (fd < 0) {
         perror("open input");
         return 1;
@@ -196,6 +261,5 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    for (;;)
-        do_one_event();
+    run_state_machine();
 }
